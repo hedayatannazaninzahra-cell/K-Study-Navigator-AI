3b526254e3c6829fec715497ffd6c6c60bd03b27
hedayatannazaninzahra-cell <hedayatannazaninzahra@gmail.com>
2026-07-07 13:11:12 +0330

    Create data.py

---
+++ data.py
@@ -0,0 +1,33 @@
+universities = [
+
+    {
+        "name": "Seoul National University",
+        "symbol": "🏛️",
+        "country": "South Korea"
+    },
+
+    {
+        "name": "KAIST",
+        "symbol": "🤖",
+        "country": "South Korea"
+    },
+
+    {
+        "name": "Yonsei University",
+        "symbol": "🎓",
+        "country": "South Korea"
+    },
+
+    {
+        "name": "Korea University",
+        "symbol": "🌸",
+        "country": "South Korea"
+    },
+
+    {
+        "name": "POSTECH",
+        "symbol": "🔬",
+        "country": "South Korea"
+    }
+
+]
